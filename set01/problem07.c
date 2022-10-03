f3c54463e4d91788855e8883f5934e7cc14913de
Akanksha Raju Raju <112924755+diamond2004k@users.noreply.github.com>
2022-10-03 04:48:43 +0000

    prob-7

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 int main(void)
+{
+  float n ,i=0 , sum=0 ;
+  printf("Enter the first n numbers\n");
+  scanf("%f" , &n);
+  while (i<=n)
+    {
+      sum = sum + i;
+        i++;
+    }
+  printf("The sum of %f is: %f" , n , sum);
+}
